6be927aeefa12b8f77b79ecca88e53ef0947bfb7
ChihYing <chihying.phone@gmail.com>
2019-05-23 18:02:08 +0800

    V0.1

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import os
 import piexif
+import PIL
 from PIL import Image
 from argparse import ArgumentParser
 from sys import stderr
@@ -7,98 +8,66 @@ from sys import stderr
 def print_err(*args, **kwargs):
     print(*args, file=stderr, **kwargs)
 
-class Fb360Converter:
-	def __init__(self, inputFile, outputFile, imgOutMaxWidth=None, imgOutRatio=None):
-		self.mInFile=inputFile
-		self.mOutFile=outputFile
-		self.mOutImgMaxWidth=imgOutMaxWidth
-		self.mOutImgRatio=imgOutRatio
+class FbPanoramaConverter:
+	FB_MAX_WIDTH=6000
+	FB_RATIO=2
+	DEFAULT_OUT_NAME = 'out.jpg'
+	
+	imgBgColor='black'
+	#>0: use specific width
+	#0: use input image's width(maxium width is FB_MAX_WIDTH)
+	#<0: something wrong
+	imgOutWidth=0
+
+	def __init__(self, imgOutWidth=None, imgBgColor=None):
+		if imgOutWidth != None and int(imgOutWidth) > 0:
+			self.imgOutWidth = int(imgOutWidth)
+		if imgBgColor != None:
+			self.imgBgColor = imgBgColor
+	
+	def convert(self, imgInName, imgOutName=None):
+		if imgOutName == None:
+			imgOutName = self.DEFAULT_OUT_NAME
+
+		#input image
+		imgIn = Image.open(imgInName)
 		
-		self.mImgIn = None
-		self.mImgInReady = False
-		
-		self.mImgOut = None
-		self.mImgOutReady = False
-		
-		if self.mOutFile == None:
-			self.mOutFile = "FB_out.jpg" 
-			
-		if self.mOutImgMaxWidth == None:
-			self.mOutImgMaxWidth = 6000
-		
-		if self.mOutImgRatio == None:
-			self.mOutImgRatio = 2 
-			
-		#print("Fb360Converter.init() args=" + self.mInFile, self.mOutFile, self.mOutImgMaxWidth, self.mOutImgRatio)
-		
-	#load input image
-	def load(self):
-		#print("Fb360Converter.load()")
-		
-		self.mImgInReady = False
-		self.mImgOutReady = False
-		try:
-			self.mImgIn = Image.open(self.mInFile)
-		except Exception as e:
-			print_err("Failed to load input image! error=", e)
-			return
-		
-		self.mImgInReady = True
-		
-	#convert image	
-	def convert(self):
-		#print("Fb360Converter.convert()")
-		
-		if not self.mImgInReady:
-			print_err("Input image is not ready! Call load() first!")
+		#output image
+		w=min(self.FB_MAX_WIDTH, imgIn.size[0])
+		if self.imgOutWidth > 0:
+			w=self.imgOutWidth
+		h=int(w/self.FB_RATIO)
+		imgOut = Image.new('RGB', (w, h), color=self.imgBgColor)
+
+		#resize input image then paste to output image
+		w = imgOut.size[0]
+		h = int(imgIn.size[1] * (imgOut.size[0]/imgIn.size[0]))
+		if w/h < self.FB_RATIO:
+			#input image's ratio is less than FB_RATIO! resize it to fit the output image
+			imgOut.paste(imgIn.resize((imgOut.size[0], imgOut.size[1]), Image.LANCZOS))
 		else:
-			#prepare output image
-			self.mImgOutReady = False
-			try:
-				w=min(self.mOutImgMaxWidth, self.mImgIn.size[0])
-				h=int(w/self.mOutImgRatio)
-				self.mImgOut=Image.new('RGB', (w, h), color = 'black')
-				
-			except Exception as e:
-				print_err("Failed to convert output image! error:", e)
-				return
-			
-			#paste input image to output image
-			if self.mImgIn.size[1] > self.mImgOut.size[1]:
-				self.mImgOut.paste(self.mImgIn.resize(self.mImgOut.size[0], self.mImgOut.size[1], Image.LANCZOS))
-			else:
-				offset = self.mImgOut.size[1] - self.mImgIn.size[1]
-				self.mImgOut.paste(self.mImgIn, (0,offset))
-				
-			exif_dict = {"0th":{271:b'RICOH', 272:b'RICOH THETA S'}}
-			self.mImgOutExif_bytes = piexif.dump(exif_dict)
-			
-			self.mImgOutReady = True
-			
-	#save image to disk
-	def save(self):
-		#print("Fb360Converter.save")
+			#resize & set offset
+			offset = imgOut.size[1] - h
+			imgOut.paste(imgIn.resize((w, h),Image.LANCZOS), (0,offset))
 		
-		if not self.mImgInReady:
-			print_err("Input image is not ready! Call load() first!")
-			return
-		elif not self.mImgOutReady:
-			print_err("Output image is not ready! Call convert() first!")
-			return
-		else:
-			self.mImgOut.save(self.mOutFile, exif=self.mImgOutExif_bytes)
-			
-						
+		#copy exif from imput image and modify manufacturer and device field
+		exif_dict = piexif.load(imgInName)
+		exif_dict["0th"][271] = b'RICOH'
+		exif_dict["0th"][272] = b'RICOH THETA S'
+		exif_bytes = piexif.dump(exif_dict)
+		
+		#save image
+		imgOut.save(imgOutName, exif=exif_bytes)
+
 
 if __name__ == '__main__':
 	argp = ArgumentParser()
 	argp.add_argument("inputImage", help="input file")
-	argp.add_argument("-o", "--output_image_filename", help="output image file name", dest="outputImg", default=None)
-	argp.add_argument("-w", "--output_max_width", help="output image max width", dest="outImgWidth", default=None)
-	argp.add_argument("-r", "--output_image_ratio", help="output image ratio", dest="outImgRatio", default=None)
+	argp.add_argument("-o", "--output", help="output image file name", dest="outputImg", default=None)
+	argp.add_argument("-w", "--width", help="output image width", dest="imgOutWidth", default=None)
+	argp.add_argument("-b", "--bg_color", help="background color", dest="imgBgColor", default=None)
 	args = argp.parse_args()
 	
-	mConv=Fb360Converter(args.inputImage, args.outputImg, args.outImgWidth, args.outImgRatio)
-	mConv.load()
-	mConv.convert()
-	mConv.save()
+	mConv=FbPanoramaConverter(args.imgOutWidth, args.imgBgColor)
+	mConv.convert(args.inputImage, args.outputImg)
+
